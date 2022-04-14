80adfd019d6fd3f7fd2e6581fabc4974d420e3e0
unknown <franzrpatricio>
2022-04-14 20:18:46 +0800

    revert changes back to normal

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-use App\Http\Controllers\BotManController;
-
-Route::match(['get', 'post'], 'botman', [BotManController::class, 'handle']);
-Route::get('/', function(){
-   return view ('index');
-});
-
-Route::get('/admin', function () {
-    return view('admin');
-});
