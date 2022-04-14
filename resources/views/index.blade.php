20f9fa5cfb1afb2ae6e08415978139328d09d105
unknown <franzrpatricio>
2022-04-14 20:04:14 +0800

    set up botman

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,13 @@
     <title>Document</title>
 </head>
 <body>
-    
-</body>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
+<script>
+var botmanWidget = {
+aboutText: 'Write Something',
+introMessage: "✋ Hi! I'm form eimpact.net"
+};
+</script>
+<script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
+<body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\BotManController;
+
+Route::match(['get', 'post'], 'botman', [BotManController::class, 'handle']);
+Route::get('/', function(){
+   return view ('index');
+});
