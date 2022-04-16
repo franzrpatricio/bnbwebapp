2cb4d902d2a133ace4588e0c43e39b262aacca5e
ajcotoner <cotoner183@gmail.com>
2022-04-16 22:58:49 +0800

    changed element tag of user module

--- resources/views/layouts/inc/admin-sidebar.blade.php
+++ resources/views/layouts/inc/admin-sidebar.blade.php
@@ -8,6 +8,33 @@
                     Dashboard
                 </a>
                 <div class="sb-sidenav-menu-heading">Interface</div>
+                
+                @if (Auth::user()->role_as == '0')
+                    {{-- ADMIN --}}
+                    
+                {{-- USER --}}
+                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUser" aria-expanded="false" aria-controls="collapseLayouts">
+                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    Users
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseUser" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link collapsed" href="{{ url('admin/add-user') }}">
+                            <div class="sb-nav-link-icon"><i class="fas fa-plus-circle"></i></div>
+                            Create New Users
+                        </a>
+                        <a class="nav-link" href="{{ url('admin/user') }}">
+                            <div class="sb-nav-link-icon"><i class="fas fa-list"></i></div>
+                            List of Users
+                        </a>
+                    </nav>
+                </div>
+                @elseif(Auth::user()->role_as == '1')
+                    {{-- STAFF --}}
+                @endif
+
+                
 
                 {{-- CATEGORIES --}}
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseCategory" aria-expanded="false" aria-controls="collapseLayouts">
@@ -46,6 +73,8 @@
                         </a>
                     </nav>
                 </div>
+
+                
 
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                     <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
