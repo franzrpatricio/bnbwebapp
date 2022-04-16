adc912e2082c0046e5d88a591739d80047fde3f5
ajcotoner <cotoner183@gmail.com>
2022-04-16 23:04:52 +0800

    create new element tag for FAQs Module

--- resources/views/layouts/inc/admin-sidebar.blade.php
+++ resources/views/layouts/inc/admin-sidebar.blade.php
@@ -74,6 +74,25 @@
                     </nav>
                 </div>
 
+                 {{-- FAQS --}}
+                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseFaqs" aria-expanded="false" aria-controls="collapseLayouts">
+                    <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                    FAQS
+                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseFaqs" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                    <nav class="sb-sidenav-menu-nested nav">
+                        <a class="nav-link collapsed" href="{{ url('admin/add-faqs') }}">
+                            <div class="sb-nav-link-icon"><i class="fas fa-plus-circle"></i></div>
+                            Create New FAQ
+                        </a>
+                        <a class="nav-link" href="{{ url('admin/faqs') }}">
+                            <div class="sb-nav-link-icon"><i class="fas fa-list"></i></div>
+                            List of FAQ
+                        </a>
+                    </nav>
+                </div>
+
                 
                 
 
