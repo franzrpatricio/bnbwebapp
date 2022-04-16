8e8c4bc088a8b580c96f1824cb155f6177b8fc52
ajcotoner <cotoner183@gmail.com>
2022-04-17 01:24:33 +0800

    Update the design pf the navbar

--- resources/views/layouts/inc/admin-navbar.blade.php
+++ resources/views/layouts/inc/admin-navbar.blade.php
@@ -1,19 +1,16 @@
-<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+<nav class="sb-topnav navbar navbar-expand" style="background: #4CA5D7">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="index.html">Bana and Bana</a>
+    <img src="{{ asset('assets/images/logo.png') }}" alt="" style="height: 5rem; width:15rem;">
     <!-- Sidebar Toggle-->
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0 text-light" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
-    <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-        <div class="input-group">
-            <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-            <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
-        </div>
-    </form>
+    <div class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+        <img src="{{ asset('assets/images/aj.jpg') }}" alt="" style="height: 3rem; width:3rem; border-radius:100rem;">
+    </div>
     <!-- Navbar-->
     <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
         <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle text-light" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 {{-- <i class="fas fa-user fa-fw"></i> --}}
                 {{-- <img src="assets/images/aj.jpg"> --}}
                 {{-- <img src="../assets/images/aj.jpg" class="rounded-circle" width="40px" height="40px"> --}}
