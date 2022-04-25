4f364935eb6c19a77af9219ae63ee05bb44f4020
ajcotoner <cotoner183@gmail.com>
2022-04-25 22:13:50 +0800

    Update the design og the profile setting. Add the activity logs in the profile settings

--- resources/views/users/profilesettings.blade.php
+++ resources/views/users/profilesettings.blade.php
@@ -4,80 +4,117 @@
 
 <div class="container light-style flex-grow-1 container-p-y">
 
-    <h4 class="font-weight-bold py-3 mb-4">
-      Account settings
-    </h4>
-    
-    <div class="card overflow-hidden">
-      <div class="row no-gutters row-bordered row-border-light">
-        <div class="col-md-3 pt-0">
-          <div class="list-group list-group-flush account-settings-links">
-            <a class="list-group-item list-group-item-action active" data-toggle="list" href="#account-general">General</a>
-            <a class="list-group-item list-group-item-action" data-toggle="list" href="#account-change-password">Change password</a>
-    
-          </div>
+  <h4 class="font-weight-bold py-3 mb-4">
+     Account settings
+  </h4>
+
+<div class="card overflow-hidden">
+   <div class="row no-gutters row-bordered row-border-light">
+      <div class="col-md-3 pt-0">
+        <div class="list-group list-group-flush account-settings-links">
+          <a class="list-group-item list-group-item-action active" data-toggle="list" href="#account-general">General</a>
+          <a class="list-group-item list-group-item-action" data-toggle="list" href="#account-change-password">Change password</a>
+          <a class="list-group-item list-group-item-action" data-toggle="list" href="#activity-logs">Activity Logs</a>
         </div>
-        <div class="col-md-9">
-          <div class="tab-content">
-            <div class="tab-pane fade active show" id="account-general">
-    <div class="text-center">
-    
-                <img class="profile-pic" src="../images/aj.jpg" alt="" style="border-radius: 100px; width: 200px; height: 200px; align-items: center; padding: 10px;">
-    
-                <div class="ml-4 sm-12">
-                  <label class="btn btn-outline-primary">
-                    Upload new photo
-                    <input type="file" class="account-settings-fileinput">
-                  </label> &nbsp;
-              </div>
+      </div>
+<div class="col-md-9">
+  <div class="tab-content">
+    <div class="tab-pane fade active show" id="account-general">
+        <div class="text-center">
+
+        <img class="profile-pic" src="../images/aj.jpg" alt="" style="border-radius: 100px; width: 200px; height: 200px; align-items: center; padding: 10px;">
+
+        <div class="ml-4 sm-12">
+          <label class="btn btn-outline-primary">
+            Upload new photo
+            <input type="file" class="account-settings-fileinput">
+          </label> &nbsp;
+      </div>
     </div>
-              
-              <hr class="border-light m-0">
-    
-              <div class="card-body">
-                <div class="form-group">
-                  <label class="form-label">Username</label>
-                  <input type="text" class="form-control mb-1" value="Enter your Username">
-                </div>
-                <div class="form-group">
-                  <label class="form-label">Name</label>
-                  <input type="text" class="form-control" value="Enter your Name">
-                </div>
-                <div class="form-group">
-                  <label class="form-label">E-mail</label>
-                  <input type="text" class="form-control mb-1" value="Enter your Email">
-                  
-                </div>
-                
-              </div>
-    
-            </div>
-            <div class="tab-pane fade" id="account-change-password">
-              <div class="card-body pb-2">
-    
-                <div class="form-group">
-                  <label class="form-label">Current password</label>
-                  <input type="password" class="form-control">
-                </div>
-    
-                <div class="form-group">
-                  <label class="form-label">New password</label>
-                  <input type="password" class="form-control">
-                </div>
-    
-                <div class="form-group">
-                  <label class="form-label">Repeat new password</label>
-                  <input type="password" class="form-control">
-                </div>
-    
-              </div>
-            </div>
+      
+      <hr class="border-light m-0">
+
+      <div class="card-body">
+        <div class="form-group">
+          <label class="form-label">Username</label>
+          <input type="text" class="form-control mb-1" value="Enter your Username">
+        </div>
+        <div class="form-group">
+          <label class="form-label">Name</label>
+          <input type="text" class="form-control" value="Enter your Name">
+        </div>
+        <div class="form-group">
+          <label class="form-label">E-mail</label>
+          <input type="text" class="form-control mb-1" value="Enter your Email">
+          
+        </div>
+        
+      </div>
+      <div class="text-right mt-3 p-3">
+        <button type="button" class="btn btn-primary">Save changes</button>&nbsp;
+        <button type="button" class="btn btn-default">Cancel</button>
+      </div>
+    </div>
+    <div class="tab-pane fade" id="account-change-password">
+      <div class="card-body pb-2">
+
+        <div class="form-group">
+          <label class="form-label">Current password</label>
+          <input type="password" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label class="form-label">New password</label>
+          <input type="password" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label class="form-label">Repeat new password</label>
+          <input type="password" class="form-control">
+        </div>
+
+      </div>
+      <div class="text-right mt-3 p-3">
+         <button type="button" class="btn btn-primary">Save changes</button>&nbsp;
+         <button type="button" class="btn btn-default">Cancel</button>
+      </div>
+    </div>
+
+   
+   
+
+
+<div class="tab-pane fade" id="activity-logs">
+      <div class="card-body pb-2">
+
+      <table class="table bg-white" >
+        <thead class="bg-dark text-light">
+            <th>ID</th>
+            <th>Name</th>
+            <th>Action</th>
+            <th>Date</th>
+        </thead>
+        <tbody>
            
-    
-    <div class="text-right mt-3 p-3">
-      <button type="button" class="btn btn-primary">Save changes</button>&nbsp;
-      <button type="button" class="btn btn-default">Cancel</button>
+            <tr>
+                <td data-title="ID">1</td>
+                <td data-title="Name">Arthur John B. Cotoner</td>
+                <td data-title="Action">Login</td>
+                <td data-title="Date">3-27-2022</td>
+
+            </tr>
+            <tr>
+                <td data-title="ID">2</td>
+                <td data-title="Name">Renz Bana</td>
+                <td data-title="Action">Login</td>
+                <td data-title="Date">3-22-2022</td>
+
+            </tr>
+           
+        </tbody>
+    </table>
+
+      </div>
     </div>
-    
-    </div>
+</div>
 @endsection
