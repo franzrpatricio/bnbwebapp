85ffcbace918a34aa41f3d02cfbefe287b5e3ade
Renzbana18 <rbana9895@gmail.com>
2022-05-13 05:22:47 +0800

    updated architect's details

--- resources/views/client/profile.blade.php
+++ resources/views/client/profile.blade.php
@@ -3,13 +3,13 @@
 
     <div class="header" style="background-image: url('{{asset('assets/client/building2.jpg')}}')">
 
-      <div  class="p-3 text-center bg-image"  style="filter: brightness(60%);">
+      <div  class="p-4 text-center bg-image"  style="filter: brightness(60%);">
         <img src="" alt="">
         </div>
       <div class="d-flex justify-content-center align-items-center" >
         <div class="banner-title text-center">
           <h1 class="mb-3 text-light">Architect's Profile</h1>
-          <p class="mb-3 text-light text-center">Get to Know our Finest Architects</p>
+          <p class="mb-3 text-light text-center" style="font-size:20px;">Get to Know our Finest Architects</p>
           
         </div>
       </div>
@@ -26,9 +26,9 @@
 				</div>
 				<div class="col-lg-8 col-md-12 col-12 ps-lg-5 mt-md-5">
 					<div class="about-text">
-						<h2 style="color:#3EA4D7;">Arch. Christopher G. Bana UAP</h2>
+						<h2 style="color:#0d6efd;">Arch. Christopher G. Bana UAP</h2>
 						<h3 style="font-size:20px; color:#ffcc00;">Co-Founder of Bana & Bana Architects</h3>
-						<p style="text-align:justify;">  Arch. Michael G. Bana is a product of Technological Institute of the Philippines graduated Bachelor of Science in Architecture in October 1992.
+						<p style="text-align:justify;">  Arch. Christopher G. Bana is a product of Technological Institute of the Philippines graduated Bachelor of Science in Architecture in October 1992.
                 He is a highly skilled and dedicated professional with 20 years of experience and has a strong background in SIte Supervising, Planning, & Technical Drawing Management.
                 Highly commited to quality and teamwork. He provides technical skills and expertise in various architectural design of engineering structures. Prepare work plans and communicate
                 to project managers to determine the scope of work. </p>
@@ -42,17 +42,17 @@
               <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
                 <i class="bx bx-cube-alt"></i>
                 <h4><a href="#">Site Supervisor</a></h4>
-                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+                <p>Been a Site Supervisor while staying in Dubai starting from 2008-2011 for a Big Architectural Firm in the country that builds infrastructures.</p>
               </div>
               <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
                 <i class="bx bx-images"></i>
                 <h4><a href="#">MEP Site Engineer</a></h4>
-                <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
+                <p>Became a Site Engineer and in charge in approving project designs and assigning engineers.</p>
               </div>
               <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
                 <i class="bx bx-shield"></i>
                 <h4><a href="#">Co-Founder of Bana & Bana Architects</a></h4>
-                <p>2011 up to the present day</p>
+                <p>Co-Founder of Bana & Bana Architects since 2011 up to the present day</p>
               </div>
             </div>
           </div>
@@ -69,10 +69,11 @@
 			<div class="row">
 				<div class="col-lg-4 col-md-12 col-12">
 					<div class="about-img "><img class="img-fluid" src="{{asset('assets/client/ar1.jpg')}}" style="height: 400px; width:700px; align-items:justify;"></div>
-				</div>
+			
+        </div>
 				<div class="col-lg-8 col-md-12 col-12 ps-lg-5 mt-md-5">
 					<div class="about-text">
-						<h2 style="color:#3EA4D7;">Arch. Michael G. Bana UAP</h2>
+						<h2 style="color:#0d6efd;">Arch. Michael G. Bana UAP</h2>
 						<h3 style="font-size:20px; color:#ffcc00;">Co-Founder of Bana & Bana Architects</h3>
 						<p style="text-align:justify;">  Arch. Michael G. Bana is a product of Technological Institute of the Philippines graduated Bachelor of Science in Architecture in October 1992.
                 He is a highly skilled and dedicated professional with 20 years of experience and has a strong background in SIte Supervising, Planning, & Technical Drawing Management.
@@ -88,17 +89,17 @@
               <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
                 <i class="bx bx-cube-alt"></i>
                 <h4><a href="#">Site Supervisor</a></h4>
-                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+                <p>Been a Site Supervisor while staying in Dubai starting from 2008-2011 for a Big Architectural Firm in the country that builds infrastructures.</p>
               </div>
               <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
                 <i class="bx bx-images"></i>
                 <h4><a href="#">MEP Site Engineer</a></h4>
-                <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
+                <p>Became a Site Engineer and in charge in approving project designs and assigning engineers.</p>
               </div>
               <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
                 <i class="bx bx-shield"></i>
                 <h4><a href="#">Co-Founder of Bana & Bana Architects</a></h4>
-                <p>2011 up to the present day</p>
+                <p>Co-Founder of Bana & Bana Architects since 2011 up to the present day</p>
               </div>
             </div>
           </div>
